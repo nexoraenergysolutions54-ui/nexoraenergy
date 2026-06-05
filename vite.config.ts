f212ae49e4ce191e1e82e5ccf1f03c9cc0d36797
bainterior20-rgb <bainterior20@gmail.com>
2026-06-05 16:44:02 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,8 +9,25 @@ export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "server" },
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+        failOnError: true,
+      },
+      pages: [
+        { path: "/" },
+        { path: "/about" },
+        { path: "/services" },
+        { path: "/industries" },
+        { path: "/projects" },
+        { path: "/hse" },
+        { path: "/news" },
+        { path: "/careers" },
+        { path: "/contact" },
+      ],
     }),
     viteReact(),
+    netlify(),
     tailwindcss(),
     tsConfigPaths(),
   ],
